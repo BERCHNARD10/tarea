40450604f263c0a09ad1a445998bbc19f0fe1615
BERCHNARD10 <20210643@uthh.edu.mx>
2024-06-06 11:10:56 -0600

    subir archivos

---
+++ static/script.js
@@ -0,0 +1,9 @@
+// Abrir modal
+document.getElementById('openModal').addEventListener('click', function() {
+    document.getElementById('myModal').style.display = 'block';
+});
+
+// Cerrar modal
+document.getElementById('closeModal').addEventListener('click', function() {
+    document.getElementById('myModal').style.display = 'none';
+});
